829b6d271a8d5502ff38aa803a5d9218003e050a
504 <504@DESKTOP-2M11V1C>
2022-02-28 18:36:11 +0900

    01

---
+++ wo/src/Day01/Day01_6.java
@@ -0,0 +1,19 @@
+package Day01;
+
+public class Day01_6 { // c s
+
+	
+	public static void main(String[] args) { // m s
+		
+		
+		
+		
+//		Scanner scanner = new Scanner(System.in);
+//		System.out.print(" 순번 : ");
+//		System.out.print(" 작성자 : ");
+//		System.out.print(" 내용 : ");
+//		System.out.print(" 날짜 : ");
+//		
+		
+	} // m e
+} // c e
