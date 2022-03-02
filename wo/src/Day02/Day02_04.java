c652ee7185725b938f1bcf31aa37ac3500fd7824
504 <504@DESKTOP-Q5RV3BV>
2022-03-02 18:23:38 +0900

    1

---
+++ wo/src/Day02/Day02_04.java
@@ -0,0 +1,14 @@
+package Day02;
+
+import java.util.Scanner;
+
+public class Day02_04 { // c s
+
+	
+	public static void main(String[] args) { // m s
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println("금액 입력");
+		
+	} // m e
+} // c e
