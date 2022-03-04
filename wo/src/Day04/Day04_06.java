277aa70e7ac200c2126405cbb3a7107c1c9e4db2
504 <504@DESKTOP-Q5RV3BV>
2022-03-04 18:09:29 +0900

    04

---
+++ wo/src/Day04/Day04_06.java
@@ -0,0 +1,55 @@
+package Day04;
+
+public class Day04_06 {
+
+	
+	public static void main(String[] args) {
+		
+		
+		* 삼목게임 [틱택토 게임 ]
+				1. 9칸 생성
+					1. String[] 배열 = new String[9]
+			2				
+
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+}
