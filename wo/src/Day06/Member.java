589d3644b7fa87325056b8a0aa20b428e761c101
504 <504@DESKTOP-Q5RV3BV>
2022-03-08 18:02:56 +0900

    06

---
+++ wo/src/Day06/Member.java
@@ -0,0 +1,33 @@
+package Day06;
+
+public class Member {
+
+	// * 회원[Member] 클래스 설계
+	
+	
+	// 1. 필드 [ 회원정보를 저장하는 메모리]
+	String id; // 
+	String password;
+	String name;
+	String phone;
+
+	// 2. 생성자
+		// 만일 생성자가 1개도 없으면 기본생성자 자동 생성
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
